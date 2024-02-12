3a27a4337e8e711d6ddae4cdb4e32031daa4ca5c
Frank Ruiz <frankruiz_90@hotmail.com>
2024-02-12 13:30:06 -0500

    first module of the react cours

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import imgReact from "../../assets/react-core-concepts.png";
+import './Header.css';
+const reactDescriptions = ["Fundamental", "Cricial", "Core"];
+
+function genRamdomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[genRamdomInt(2)];
+  return (
+    <header>
+      <img src={imgReact} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
